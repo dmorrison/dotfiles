d585027f9e8795b4451ea389f42317a661881efb
Derek Morrison <me@derek-morrison.com>
2025-05-05 13:31:47 -0500

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -4,8 +4,10 @@ clear
 
 echo "Starting install script..."
 
-echo "Installing command-line-developer tools..."
-xcode-select --install
+echo "Installing Homebrew..."
+# Note: The Homebrew installation script installs the Command Line Developer Tools
+# if they're not already installed.
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "Setup complete!"
 echo

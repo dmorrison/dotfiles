ca45960d8f265e1f930e8ddca3954c9f149c79d7
Derek Morrison <me@derek-morrison.com>
2025-05-05 13:37:10 -0500

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -4,11 +4,20 @@ clear
 
 echo "Starting install script..."
 
+echo "Getting sudo access..."
+sudo -v
+
+echo "Keeping sudo access until the script is finished..."
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done 2>/dev/null &
+
 echo "Installing Homebrew..."
 # Note: The Homebrew installation script installs the Command Line Developer Tools
 # if they're not already installed.
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "Setup complete!"
-echo
 echo

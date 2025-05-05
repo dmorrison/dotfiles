21196c2ca2965e1418f4f02d534034f6215dfd7d
Derek Morrison <me@derek-morrison.com>
2025-05-05 13:13:12 -0500

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -2,4 +2,11 @@
 
 clear
 
-echo "Please work!!!"
+echo "Starting install script...
+
+echo "Installing command-line-developer tools..."
+xcode-select --install
+
+echo "Setup complete!"
+echo
+echo
